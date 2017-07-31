69d14f2b932107fdd961c8647223c5a001a26ea2
Bo Hu <bhu@mesosphere.com>
2017-07-31 11:21:23 -0700

    remove the dir detection

--- common/fit.py
+++ common/fit.py
@@ -36,9 +36,6 @@ def _load_model(args, rank=0):
 def _save_model(args, rank=0):
     if args.model_prefix is None:
         return None
-    dst_dir = os.path.dirname(args.model_prefix)
-    if not os.path.isdir(dst_dir):
-        os.mkdir(dst_dir)
     return mx.callback.do_checkpoint(args.model_prefix if rank == 0 else "%s-%d" % (
         args.model_prefix, rank))
 
